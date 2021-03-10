3415305cbc921deb72ddd5ae5badc8a1581f04de
yusuf963 <mohammad963yusuf@gmail.com>
2021-03-10 16:29:47 +0000

    creating user/item schema

---
+++ server/models/item.py
@@ -0,0 +1,23 @@
+from app import db
+from models.base import BaseModel
+from models.user import User
+
+
+class Cake(db.Model, BaseModel):
+
+    __tablename__ = "item"
+
+    title = db.Column(db.String(40), nullable=False, unique=True)
+    category = db.Column(db.String(40), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    note = db.Column(db.String(200), nullable=True)
+    image = db.Column(db.Text, nullable=False)
+    # booking = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id', ondelete="CASCADE"))
+
+    booking = db.relationship(
+        'Booking', backref='user', cascade="all, delete")
+
+    # ingredients = db.relationship(
+    #     'Ingredient', backref='cakes', secondary=cakes_ingredients_join)
---
+++ server/models/user.py
@@ -0,0 +1,52 @@
+from app import db, bcrypt
+
+from models.base import BaseModel
+from sqlalchemy.ext.hybrid import hybrid_property
+import jwt
+from datetime import *
+from config.environment import secret
+
+
+class User(db.Model, BaseModel):
+
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(15), nullable=False, unique=True)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    address = db.Column(db.Text, nullable=False, unique=True)
+    profile_pic = db.Column(db.String(200), nullable=True)
+    rating = db.Column(db.String(200), nullable=True)
+    password_hash = db.Column(db.String(128), nullable=True)
+
+    # ? Create a relationship field to comments
+    notification = db.relationship(
+        'Notification', backref='user', cascade="all, delete")
+    # ? Create a relationship field to cakes
+    message = db.relationship('Message', backref='user', cascade="all, delete")
+
+    # ! The equivalent of a virtual field in sqlalchemy
+    # ! This is a temporary field that should be not be saved in db
+
+    @hybrid_property
+    def password(self):
+        pass
+
+    @password.setter
+    def password(self, password_plaintext):
+        encoded_pw = bcrypt.generate_password_hash(password_plaintext)
+        self.password_hash = encoded_pw.decode('utf-8')
+
+    # !  password Validation
+    def validate_password(self, password_plaintext):
+        return bcrypt.check_password_hash(self.password_hash, password_plaintext)
+
+    # ! Generate a token
+    def generate_token(self):
+        payload = {
+            "sub": self.id,
+            "iat": datetime.utcnow(),
+            "exp": datetime.utcnow() + timedelta(days=1)
+        }
+        token = jwt.encode(payload, secret, 'HS256')
+
+        return token
